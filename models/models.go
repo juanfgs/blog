a92608c40261914989c3b567e6d6d7a398e8cefe
Juan Francisco Giménez Silva <juanfgs@gmail.com>
2015-08-30 22:54:58 -0300

    added settings model

--- models/models.go
+++ models/models.go
@@ -15,6 +15,7 @@ func init(){
 	orm.RegisterModel(new(Post))
 	orm.RegisterModel(new(Comment))
 	orm.RegisterModel(new(Media))
+	orm.RegisterModel(new(Setting))	
 	dbpass := beego.AppConfig.String("dbpass")
 	dbuser := beego.AppConfig.String("dbuser")
 	dbhost := beego.AppConfig.String("dbhost")
