2c0cfd885c7d5c787673856da158507cb0f7abbd
Juan F. Giménez Silva <juanfgs@gmail.com>
2016-01-03 22:02:15 -0300

    added feeds for individual categories

--- routers/router.go
+++ routers/router.go
@@ -14,13 +14,14 @@ func init() {
 	beego.Router("/comment/new", &controllers.CommentsController{}, "post:CommentWrite")
 	beego.Router("/categories/:id([0-9]+)", &controllers.CategoriesController{}, "get:Show")
 
+
 	beego.Router("/login", &controllers.UsersController{}, "get:Login")
 	beego.Router("/login/process", &controllers.UsersController{}, "post:LoginPost")
 	beego.Router("/logout", &controllers.UsersController{}, "get:Logout")
 	beego.Router("/register/process", &controllers.UsersController{}, "post:RegisterPost")
 
 	beego.Router("/feed.xml", &controllers.FeedsController{}, "get:Index")
-
+	beego.Router("/categories/:id([0-9]+)/feed.xml", &controllers.FeedsController{}, "get:Category")
 	//	beego.Router("/images/:height([0-9])x:width[0-9]/:image:string", &controllers.ThumbnailsController{}, "get:showImage")
 
 }
